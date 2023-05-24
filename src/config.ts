0ed5b7bac27d05408b04d1aa623eb309bc365486
Lucas Baumgart <lzbaumgart@gmail.com>
2023-05-24 13:51:31 -0300

    adicionado constantes exigidas

--- src/config.ts
+++ src/config.ts
@@ -109,3 +109,20 @@ export const NavigationLinks = [
   { name: 'Contato', href: 'contact' },
   //{ name: 'Blog', href: 'blog' },
 ]
+
+export const PAGE_SIZE = 6
+
+export const GITHUB_EDIT_URL = `https://github.com/hellotham/hello-astro`
+
+export const COMMUNITY_INVITE_URL = `https://astro.build/chat`
+
+export type Sidebar = Record<string, { text: string; link: string }[]>
+
+export const SIDEBAR: Sidebar = {
+  'Section Header': [
+    { text: 'Introduction', link: 'doc/introduction' },
+    { text: 'Page 2', link: 'doc/page-2' },
+    { text: 'Page 3', link: 'doc/page-3' },
+  ],
+  'Another Section': [{ text: 'Page 4', link: 'doc/page-4' }],
+}
